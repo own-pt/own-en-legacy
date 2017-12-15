715c181890003ccf7529d45bfd987f59b52bfb36
Fabricio Chalub <fchalub@br.ibm.com>
2017-12-15 15:17:20 -0200

    exporting used functions.

--- src/package.lisp
+++ src/package.lisp
@@ -2,6 +2,8 @@
 (defpackage #:wordnet-dsl
   (:use #:cl :cl-ppcre #:alexandria)
   (:export #:read-wn
+           #:get-sem-pointer
+           #:synset-senses
 	   #:make-local-links
 	   #:make-external-links
 	   #:txt->rdf))
