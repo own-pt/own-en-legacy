cc5bc4d0416dfb45fa3c6aefd3ffe4c303b9e8ce
Fabricio Chalub <fchalub@br.ibm.com>
2018-05-14 13:07:24 -0300

    beginnings of synset display; added interactive examples.

--- notebook/ownen.py
+++ notebook/ownen.py
@@ -5,10 +5,11 @@ import json, requests
 from rdflib import Graph
 from rdflib.namespace import Namespace, NamespaceManager
 from rdflib.term import URIRef
+from IPython.core.display import HTML, Markdown
 
 class OwnEn:
     
-    def __init__(self,endpoint = 'http://localhost/sparql'):
+    def __init__(self,endpoint = 'http://localhost/own-en/'):
         self.endpoint = endpoint
         self.namespaceManager = NamespaceManager(Graph())
         self.namespaceManager.bind('schema', Namespace('https://br.ibm.com/tkb/own-en/schema/'), override=False)
@@ -65,7 +66,33 @@ class OwnEn:
     def hyponym(self, s):
         return self.relation_query('schema:hyponymOf', s)
 
+    def show(self, s):
+        query = """prefix inst: <https://br.ibm.com/tkb/own-en/instances/> 
+        prefix schema: <https://br.ibm.com/tkb/own-en/schema/>
+        select ?l (group_concat(?o) as ?go)
+        {{
+            {} ?p ?o .
+            ?o a schema:Synset .
+            ?p rdfs:label ?l .
+            filter (?p != rdf:type) .
+        }}
+        group by ?l
+        order by ?l"""
+        
+        response = requests.post(self.endpoint,
+                                 headers = {'Accept': 'application/sparql-results+json'},
+                                 data = {'query': query.format(s)})
+
+        values = [(x['l']['value'],x['go']['value']) for x in response.json()['results']['bindings']]
+        result = ""
+        for v in values:
+            
+            result += "- {}: {}\n".format(v[0]," ".join([self.namespaceManager.qname(URIRef(x)) for x in v[1].split()]))
+
+        return Markdown(result)
+    
 if __name__ == "__main__":
-    t = OwnEn()
-    print(t.search("carbonate"))
+    en = OwnEn()
+    print(en.show('inst:synset-noun.artifact-chair2'))
+    
     
